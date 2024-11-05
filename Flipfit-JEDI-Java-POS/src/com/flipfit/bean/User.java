326efe647078b5c22e71b3687257f74b52b0379e
Riturajrs <rituraj2001.rs@gmail.com>
2024-11-05 17:32:54 +0530

    User rebase fixed

--- Flipfit-JEDI-Java-POS/src/com/flipfit/bean/User.java
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/bean/User.java
@@ -1,25 +1,16 @@
 package com.flipfit.bean;
 
-<<<<<<< HEAD
-public class User {
-    private String userId;
-=======
 public class User extends Person {
-    private int userId;
->>>>>>> 878e862 (Added a super class for admin, user and gym owner, along with interfaces)
+    private String userId;
     private String userPhone;
     private double userWeight;
     private int[] userSlots;
 
-<<<<<<< HEAD
-    public String getUserId() {
-=======
     public User(String id, String name, String email, String password, String firstName, String lastName) {
-        super(id, name, email, password, "ADMIN", firstName, lastName);
+        super(id, name, email, password, "USER", firstName, lastName);
     }
 
-    public int getUserId() {
->>>>>>> 878e862 (Added a super class for admin, user and gym owner, along with interfaces)
+    public String getUserId() {
         return userId;
     }
 
