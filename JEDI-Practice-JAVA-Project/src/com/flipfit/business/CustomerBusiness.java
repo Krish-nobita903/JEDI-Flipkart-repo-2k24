e4cab87c23205f4eebc68f9fcb66b38c6f358d6d
Krish-nobita903 <105687969+Krish-nobita903@users.noreply.github.com>
2024-11-05 12:40:43 +0530

    Delete JEDI-Practice-JAVA-Project directory

--- JEDI-Practice-JAVA-Project/src/com/flipfit/business/CustomerBusiness.java
+++
@@ -1,8 +0,0 @@
-package com.flipfit.business;
-
-public interface CustomerBusiness {
-    public void createCustomer();
-    public boolean updateCustomer(int customerId);
-    public boolean deleteCustomer(int customerId);
-    public void listCustomers();
-}
