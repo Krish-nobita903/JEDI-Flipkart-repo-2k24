c68252f8abf7bd53123c74ca999e415b7bbb8acd
Riturajrs <rituraj2001.rs@gmail.com>
2024-11-05 17:25:45 +0530

    Added payment entity, with interface and dummy implementation

---
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/bean/Payment.java
@@ -0,0 +1,22 @@
+package com.flipfit.bean;
+
+public class Payment {
+    private String paymentDetails;
+    private String paymentType;
+
+    public String getPaymentDetails() {
+        return paymentDetails;
+    }
+
+    public void setPaymentDetails(String paymentDetails) {
+        this.paymentDetails = paymentDetails;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+}
