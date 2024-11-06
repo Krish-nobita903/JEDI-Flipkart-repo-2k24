556ed1aaee70380ca00f357f0a89dab43613e8c2
Krish-nobita903 <krish.rb.2517@gmail.com>
2024-11-06 11:01:04 +0530

    Adding menu cases in flipfit app

--- Flipfit-JEDI-Java-POS/src/com/flipfit/application/FlipFitApp.java
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/application/FlipFitApp.java
@@ -13,20 +13,63 @@ public class FlipfitApp {
     {
         GymManagerInterface manager = new GymManagerImpl();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to Flipfit");
-        System.out.println("Kindly select your role:");
-        System.out.println("1.Gym Owner");
-        System.out.println("2.Customer");
+        System.out.println("Welcome to the FLIPFIT APP");
+        System.out.println("1.Login");
+        System.out.println("2.Rgeisteration for GYMCustomer");
+        System.out.println("3.Rgeisteration for GYMOwner");
+        System.out.println("4.Update Password");
+        System.out.println("5.Exit");
 
-        int roleChosen = scanner.nextInt();
-        scanner.nextLine();// store newline
+        int choice = scanner.nextInt();
+        scanner.nextLine();
 
-        switch (roleChosen) {
-            case 1 -> handleGymOwnerActions(scanner,manager);
-            case 2 -> handleCustomerActions(scanner);
-            default -> System.out.println("Invalid choice. Exiting application.");
+        switch(choice){
+            case 1:
+                System.out.println("Please enter your username:");
+                String userName =  scanner.nextLine();
+                scanner.nextLine();
+                System.out.println("Please enter your Password:");
+                String password = scanner.nextLine();
+                scanner.nextLine();
+                System.out.println("Please enter your Role:");
+                System.out.println("1.Choose 1 for Admin:");
+                System.out.println("2.Choose 2 for GymOwner:");
+                System.out.println("3.Choose 3 for GymCustomer:");
+                int roleChosen = scanner.nextInt();
+                scanner.nextLine();// store newline
+
+                switch (roleChosen) {
+                    case 1 -> handleAdminActions(scanner);
+                    case 2 -> handleGymOwnerActions(scanner);
+                    case 3 -> handleCustomerActions(scanner);
+                    default -> System.out.println("Invalid choice. Exiting application.");
+                }
+                break;
+            case 2:
+                System.out.println("You have entered Gym Customer Registeration");
+                break;
+
+            case 3:
+                System.out.println("You have entered Gym Owner Customer");
+                break;
+
+            case 4:
+                System.out.println("You have entered Update Password");
+                System.out.println("Please enter your username:");
+                String userNameForUpdatePassword =  scanner.nextLine();
+                scanner.nextLine();
+                System.out.println("Please enter your Old Password:");
+                String oldPassword = scanner.nextLine();
+                scanner.nextLine();
+                System.out.println("Please enter your New Password:");
+                String newPassword = scanner.nextLine();
+                scanner.nextLine();
+                updatePassword(scanner,userNameForUpdatePassword,oldPassword,newPassword);
+                break;
+
+            default:
+                break;
         }
-
         scanner.close();
     }
 
@@ -34,16 +77,18 @@ public class FlipfitApp {
     {
         System.out.println("Welcome Customer!");
         System.out.println("Choose your option");
-        System.out.println("1.View Slot");
-        System.out.println("2.Book Slot");
-        System.out.println("3.Cancel Booked Slots");
-        System.out.println("4.Update User Info");
+        System.out.println("1.View Booked Slot");
+        System.out.println("2.View Available Slot");
+        System.out.println("3.Book Slot");
+        System.out.println("4.Cancel Booked Slots");
+        System.out.println("5.Update User Info");
 
         int optionSelected = scanner.nextInt();
         scanner.nextLine();
 
         switch (optionSelected){
-            case 1 -> ViewSlots();
+            case 1 -> ViewBookedSlots();
+            case 2 -> ViewAvailableSlots();
             case 2 -> BookSlots();
             case 3 -> CancelBookedSlot();
             case 4 -> UpdateUserInfo();
@@ -71,4 +116,23 @@ public class FlipfitApp {
             default -> System.out.println("Invalid choice. Exiting application.");
         }
     }
+
+    private static void handleAdminActions(Scanner scanner)
+    {
+        System.out.println("Welcome Customer!");
+        System.out.println("Choose your option");
+        System.out.println("1.Add User");
+        System.out.println("2.Add GymOwner");
+
+        int optionSelected = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (optionSelected){
+            case 1 -> AddUser();
+            case 2 -> AddGymOwner();
+            default -> System.out.println("Invalid choice. Exiting application.");
+        }
+
+    }
+
 }
