45843051d681a78853a7cdc4a58d68915ebe295c
Riturajrs <rituraj2001.rs@gmail.com>
2024-11-07 18:53:51 +0530

    Add bulk insert for slots while registering new gym

--- Flipfit-JEDI-Java-POS/src/com/flipfit/dao/GymDAO.java
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/dao/GymDAO.java
@@ -40,7 +40,39 @@ public class GymDAO implements GymDAOInterface{
 
     @Override
     public boolean updateGym(Gym gym) {
-        return false;
+        try{
+            Connection connection = DatabaseConnection.connect();
+            connection.setAutoCommit(false);
+
+            PreparedStatement stmtForGym = connection.prepareStatement("INSERT INTO FlipfitSchema.gym (id,regionId,pincode) values(?,?,?)");
+            stmtForGym.setString(1, gym.getId());
+            stmtForGym.setString(2, gym.getRegionId());
+            stmtForGym.setString(3, gym.getPincode());
+            stmtForGym.executeUpdate();
+            System.out.println("New gym created with gym id: " + userId);
+
+            PreparedStatement stmtForSlot = connection.prepareStatement("INSERT INTO FlipfitSchema.slot (slotId,gymId,startTime,date,availableSeats) " +
+                    "values(?,?,?)");
+
+            // Bulk insert for slots
+            for( Slot slot : slotsAvailable) {
+                stmtForSlot.setString(1, slot.getId());
+                stmtForSlot.setString(2, gym.getId());
+                stmtForSlot.setString(3, slot.getStartTime());
+                stmtForSlot.setDate(4, slot.getDate());
+                stmtForSlot.setInt(5, slot.getAvailableSeats());
+                stmtForSlot.addBatch();
+            }
+            stmtForSlot.executeBatch();
+            connection.commit();
+            stmt.close();
+            connection.close();
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
