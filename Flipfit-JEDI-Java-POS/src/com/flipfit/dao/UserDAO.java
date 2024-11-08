4306813ca2fcf856890fd1cc02df8f9f66e805f5
Riturajrs <rituraj2001.rs@gmail.com>
2024-11-08 13:13:18 +0530

    Added login for customer

--- Flipfit-JEDI-Java-POS/src/com/flipfit/dao/UserDAO.java
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/dao/UserDAO.java
@@ -6,8 +6,27 @@ import com.flipfit.helper.DatabaseConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.UUID;
+import java.sql.ResultSet;
 
-public class UserDAO implements UserDAOInterface{
+public class UserDAO implements UserDAOInterface, LoginInterface{
+
+    @Override
+    public boolean login(String username, String password){
+        try{
+            Connection connection = DatabaseConnection.connect();
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM FlipfitSchema.user WHERE userName = ? AND password = ?");
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if( rs.next() ){
+                return true;
+            }
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
+        return false;
+    }
+
     @Override
     public boolean createUser(String userName, String email, String password, String firstName, String lastName) {
         try{
