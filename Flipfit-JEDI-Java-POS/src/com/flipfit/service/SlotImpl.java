c0e5fe1263aca0f47e72dbb17df05ce500f2870f
napan24 <napanv24@gmail.com>
2024-11-10 15:20:19 +0530

    added Slot Impl

--- Flipfit-JEDI-Java-POS/src/com/flipfit/service/SlotImpl.java
+++ Flipfit-JEDI-Java-POS/src/com/flipfit/service/SlotImpl.java
@@ -14,10 +14,14 @@ public class SlotImpl implements SlotInterface {
     public boolean addSlot(Slot slot) {
         try {
             // add impl and change if condition
-            Boolean isSlotAdded = true;
-            if(!isSlotAdded){
+            slotDAO.addSlot(slot);
+            int id = slot.getSlotId();
+            String SlotId = String.valueOf(id);
+            Slot newSlot = slotDAO.getSlot(SlotId);
+            if(newSlot == null){
                 throw new SlotInesrtionFailedException();
             }
+            return true;
         }
         catch (SlotInesrtionFailedException e) {
             System.out.println(e.getMessage());
@@ -32,13 +36,18 @@ public class SlotImpl implements SlotInterface {
     public boolean updateSlot(Slot slot) {
         try {
             // add impl and change if condition
-            Boolean isSlotUpdated = true;
-            if(!isSlotUpdated){
+            int id = slot.getSlotId();
+            String SlotId = String.valueOf(id);
+            Slot oldSlot = slotDAO.getSlot(SlotId);
+            slotDAO.updateSlot(oldSlot, slot);
+            Slot newSlot = slotDAO.getSlot(SlotId);
+            if(!oldSlot.equals(newSlot)){
                 throw new UpdateFailedException();
             }
+            return true;
         }
         catch (UpdateFailedException e) {
-
+            System.out.println(e.getMessage());
         }
         finally {
             System.out.println("Slot has been updated!");
@@ -49,10 +58,14 @@ public class SlotImpl implements SlotInterface {
     @Override
     public boolean deleteSlot(Slot slot) {
         try {
-            Boolean isSlotDeleted = true;
-            if(!isSlotDeleted){
+            slotDAO.deleteSlot(slot);
+            int slotId = slot.getSlotId();
+            String SlotId = String.valueOf(slotId);
+            Slot deletedSlot = slotDAO.getSlot(SlotId);
+            if(deletedSlot != null){
                 throw new DeletionFailedException();
             }
+            return true;
         }
         catch (DeletionFailedException e) {
             System.out.println(e.getMessage());
