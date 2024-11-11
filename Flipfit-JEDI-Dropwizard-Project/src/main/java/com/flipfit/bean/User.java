a9ccf9275cea83ccb5f3a0b85e6d0287512c78f1
vkm411101 <vkm411101@gmail.com>
2024-11-11 17:08:30 +0530

    Adds User Related Files, Might need Refactor

---
+++ Flipfit-JEDI-Dropwizard-Project/src/main/java/com/flipfit/bean/User.java
@@ -0,0 +1,38 @@
+package com.flipfit.bean;
+
+public class User extends Person{
+    private String userPhone;
+    private double userWeight;
+    private int[] userSlots;
+
+    public User(String id, String name, String email, String password, String firstName, String lastName) {
+        super(id, name, email, password, "USER", firstName, lastName);
+    }
+    public User(){
+        super("", "", "", "", "", "", "");
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public double getUserWeight() {
+        return userWeight;
+    }
+
+    public void setUserWeight(double userWeight) {
+        this.userWeight = userWeight;
+    }
+
+    public int[] getUserSlots() {
+        return userSlots;
+    }
+
+    public void setUserSlots(int[] userSlots) {
+        this.userSlots = userSlots;
+    }
+}
---
+++ Flipfit-JEDI-Dropwizard-Project/src/main/java/com/flipfit/dao/LoginInterface.java
@@ -0,0 +1,7 @@
+package com.flipfit.dao;
+
+import com.flipfit.bean.Gym;
+
+public interface LoginInterface {
+    public String login(String username, String password);
+}
