befb5790c8b4c4be98955710aae2d73ec96efb7f
Krish-nobita903 <krish.rb.2517@gmail.com>
2024-11-11 15:40:38 +0530

    Adds dropWizard services

---
+++ Flipfit-JEDI-Dropwizard-Project/target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.flipfit.app"},{"l":"com.flipfit.constants"},{"l":"com.flipfit.dao"},{"l":"com.flipfit.exception"},{"l":"com.flipfit.restcontroller"},{"l":"com.flipfit.service"},{"l":"com.flipfit.utils"},{"l":"org.example"}];updateSearchResults();
---
+++ Flipfit-JEDI-Dropwizard-Project/target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.flipfit.app","l":"App"},{"p":"org.example","l":"Main"}];updateSearchResults();
